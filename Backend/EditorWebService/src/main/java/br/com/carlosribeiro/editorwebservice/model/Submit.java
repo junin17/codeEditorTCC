76afaa933a6e2ed0308325930bcddd48ed5afbc0
junin17 <carlosarju@gmail.com>
2018-04-20 00:20:13 -0300

    Estruturando o projeto

---
+++ Backend/EditorWebService/src/main/java/br/com/carlosribeiro/editorwebservice/model/Submit.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.carlosribeiro.editorwebservice.model;
+
+/**
+ *
+ * @author carlos.ribeiro
+ */
+public class Submit {
+    private String code;
+    private String linguagem;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getLinguagem() {
+        return linguagem;
+    }
+
+    public void setLinguagem(String linguagem) {
+        this.linguagem = linguagem;
+    }
+    
+    
+    
+}
---
+++ Backend/EditorWebService/src/main/java/br/com/carlosribeiro/editorwebservice/services/EditorService.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.carlosribeiro.editorwebservice.services;
+
+import br.com.carlosribeiro.editorwebservice.model.Submit;
+import com.google.gson.Gson;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * REST Web Service
+ *
+ * @author carlos.ribeiro
+ */
+@Path("editor")
+public class EditorService {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of EditorService
+     */
+    public EditorService() {
+    }
+
+    /**
+     * Retrieves representation of an instance of br.com.carlosribeiro.editorwebservice.services.EditorService
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response getText() {
+        //TODO return proper representation object
+        return Response.ok() //200
+			.entity("tamo junto")
+			.header("Access-Control-Allow-Origin", "*")
+			.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+			.allow("OPTIONS").build();
+    }
+
+    /**
+     * PUT method for updating or creating an instance of EditorService
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.TEXT_PLAIN)
+    public void putText(String content) {
+    }
+    
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response postCodigo(String content){
+        Gson gson = new Gson();
+        
+        Submit submit = gson.fromJson(content, Submit.class);
+        System.out.println(submit.getCode());
+        
+        return Response.ok() //200
+			.entity(content)
+			.header("Access-Control-Allow-Origin", "*")
+			.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT")
+			.allow("OPTIONS").build();
+    }
+}
